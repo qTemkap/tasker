d09bf15ac9cd5dde2389135ae3667717dcead569
ArtemSabelnikov <artem.s@unitspace.top>
2021-03-17 12:31:45 +0200

    login

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,13 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    echo "asdasd";
-
-//    return view('welcome');
+    return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
